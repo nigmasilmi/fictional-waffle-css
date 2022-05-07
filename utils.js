3a358c7795291d26668728d05915d5c2bbb0eeb4
Nigma Silmi <sn.silmi@gmail.com>
2022-05-07 17:24:38 -0400

    Grid and animations added

--- utils.js
+++ utils.js
@@ -6,11 +6,14 @@ const modal = document.querySelector(".modal");
 const toggleSideBarBtn = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
 
+const ctaBtn = document.querySelector(".main-nav__item--cta");
+
 for (let i = 0; i < selectPlanBtns.length; i++) {
   selectPlanBtns[i].addEventListener("click", function () {
-    // backdrop.style.display = "block";
-    // modal.style.display = "block";
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(() => {
+      backdrop.classList.add("open");
+    }, 10);
     modal.classList.add("open");
   });
 }
@@ -19,29 +22,37 @@ for (let i = 0; i < selectPlanBtns.length; i++) {
 const noBtn = document.querySelector("button.modal__action--negative");
 if (noBtn) {
   noBtn.addEventListener("click", function () {
-    //   backdrop.style.display = "none";
-    //   modal.style.display = "none";
-    backdrop.classList.remove("open");
+    backdrop.style.display = "block";
     modal.classList.remove("open");
+    setTimeout(() => {
+      backdrop.classList.remove("open");
+    }, 10);
   });
 }
 
 // Closing modal when clicking on backdrop
 backdrop.addEventListener("click", function () {
-  //   backdrop.style.display = "none";
-  //   modal.style.display = "none";
-  //   mobileNav.style.display = "none";
-  backdrop.classList.remove("open");
   if (modal) {
     modal.classList.remove("open");
   }
+  backdrop.classList.remove("open");
+  setTimeout(() => {
+    backdrop.style.display = "none";
+  }, 200);
   mobileNav.classList.remove("open");
 });
 
 toggleSideBarBtn.addEventListener("click", function () {
-  //   mobileNav.style.display = "block";
-  //   backdrop.style.display = "block";
-
   backdrop.classList.add("open");
   mobileNav.classList.add("open");
+});
+
+ctaBtn.addEventListener("animationstart", function (event) {
+  console.log("animation STARTED", event);
+});
+ctaBtn.addEventListener("animationiteration", function (event) {
+  console.log("ITERATED", event);
+});
+ctaBtn.addEventListener("animationend", function (event) {
+  console.log("animation ENDED", event);
 });
